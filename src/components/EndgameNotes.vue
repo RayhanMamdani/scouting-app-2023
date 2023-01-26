5df13b94c6e1844c1122c213c7698ffa9c7fc2f0
alidakanji <alkanji@bayviewglen.ca>
2023-01-26 16:27:52 -0500

    Created Endgame notes

---
+++ src/components/EndgameNotes.vue
@@ -0,0 +1,4 @@
+<template>
+    <p>General Scouting Notes</p>
+    <textarea class="textarea" placeholder="Type here..."></textarea>
+</template>
--- src/views/EndgameView.vue
+++ src/views/EndgameView.vue
@@ -1,12 +1,21 @@
 <script setup>
     import TableContents from '../components/TableContents.vue';
+    import EndgameNotes from '../components/EndgameNotes.vue';
+    import Chargestation from '../components/Chargestation.vue';
 </script>
 
 <template>
     <main>
-        <p>test</p>
-        <div>
-            <TableContents />
+        <div class="columns">
+            <div class="column">
+                <TableContents />
+            </div>
+            <div class="column">
+                <EndgameNotes/>
+            </div>
+            <div class="column">
+                <Chargestation/>
+            </div>
         </div>
     </main>
 </template>
