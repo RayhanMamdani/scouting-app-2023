2f14e17a5271c8ae3b3ec1090816abca6ee7f6b9
jdimenna <jdimenna@bayviewglen.ca>
2023-01-26 16:54:41 -0500

    removed center from pickup.vue

--- src/components/Pickup.vue
+++ src/components/Pickup.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="pickup">
-        <center>
-            <p style="font-size:25px;">Pickup</p>
-            <div class="buttons has-addons is-centered">
-                <button class="button is-warning is-large is-responsive">Substation Pickup Top</button>
-                <button class="button is-warning is-large is-responsive">Substation Pickup Bottom</button>
-                <button class="button is-warning is-large is-responsive">Ground Pickup</button>
-            </div>
-        </center>
+        <p style="font-size:25px;">Pickup</p>
+        <div class="buttons has-addons is-centered">
+            <button class="button is-warning is-large is-responsive">Substation Pickup Top</button>
+            <button class="button is-warning is-large is-responsive">Substation Pickup Bottom</button>
+            <button class="button is-warning is-large is-responsive">Ground Pickup</button>
+        </div>
     </div>
 </template>
