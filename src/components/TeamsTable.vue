40e691dfa7f4777c8c2f66d9b1e8fd298f4fae86
trevor-cheung <tcheung@bayviewglen.ca>
2023-04-05 19:52:57 -0400

    made grid fixed at all viewing breakpoints

--- src/components/TeamsTable.vue
+++ src/components/TeamsTable.vue
@@ -330,7 +330,7 @@ import TeamDataService from '../services/TeamDataService.js'
         this.createTeams();
         this.sort("teamNum");
     },
-    components: { AddTeams }
+    
 }
     /*const DeleteButton = defineCustomElement({
         props: {
