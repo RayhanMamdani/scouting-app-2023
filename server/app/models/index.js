8080bc20c54cdeb4c90256adec7432bd1032f78e
Brandon Tai <btai@bayviewglen.ca>
2023-09-27 16:43:07 -0400

    localhost server

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,7 +1,15 @@
 const dbConfig = require("../config/db.config.js");
-
+const express = require('express');
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
 mongoose.Promise = global.Promise;
+const app = express();
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const db = {};
 db.mongoose = mongoose;
