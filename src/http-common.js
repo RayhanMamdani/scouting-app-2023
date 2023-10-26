a1dcdb6fe1d547279090e1e6540ed4678e97becb
Brandon Tai <btai@bayviewglen.ca>
2023-10-26 19:14:25 -0400

    build

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api", // change to 'baseURL: ""https://frc.a193.ca/api when building"
+  baseURL: "https://seven558serverplswork.onrender.com/api", // change to 'baseURL: ""https://frc.a193.ca/api when building"
   headers: {
     "Content-type": "application/json"
   }
